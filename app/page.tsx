d87f5d89caebf4d7596300a1e6ecec0eade88570
DouglasUtf <133456856+DouglasUtf@users.noreply.github.com>
2023-06-16 23:02:50 +0000

    Save

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <h2 className='text-center'>Home from  Douglas </h2>
+    <h2 className='text-center'>Home from  Douglas.... </h2>
   )
 }
